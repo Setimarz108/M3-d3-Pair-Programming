6b4f0833b13b388219fa5e4cd0912bcc2e883f50
stefantanasa <liketanasa@gmail.com>
2021-12-01 16:16:55 +0000

    update

--- M3-d3/app.js
+++ M3-d3/app.js
@@ -10,33 +10,31 @@ const getPhotos = () => {
   })
     .then((response) => response.json())
     .then((photoData) => {
-      console.log(photoData.photos[0].url);
-
       for (let i = 0; i < getThumbnail.length; i++) {
         // new image
+        console.log(i);
         let cardImg = document.createElement("img");
         cardImg.className = "bd-placeholder-img card-img-top";
-        cardImg.src = `${photoData.photos[i].url}`;
+        cardImg.src = `${photoData.photos[i].src.medium}`;
 
         getThumbnail[i].firstChild.remove();
-
         getThumbnail[i].replaceChild(cardImg, getThumbnail[i].firstChild);
+        console.log(getThumbnail[i].firstChild);
       }
     });
 };
 
-getPhotos();
 // get the button and add event
 let getButton = document.querySelector(".btn.btn-primary.my-2");
 getButton.addEventListener("click", getPhotos);
 let getThumbnail = document.querySelectorAll(".col-md-4> .card.mb-4.shadow-sm");
 
-// // new image
+// new image
 // let cardImg = document.createElement("img");
 // cardImg.className = "bd-placeholder-img card-img-top";
 // cardImg.src = "https://pixlr.com/images/index/remove-bg.webp";
 
-// replace svg with photo
+// // replace svg with photo
 // getThumbnail[0].firstChild.remove();
 // getThumbnail[0].replaceChild(cardImg, getThumbnail[0].firstChild);
 // console.log(getThumbnail[0].firstChild);
