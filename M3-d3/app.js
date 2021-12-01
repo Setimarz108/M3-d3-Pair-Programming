71398a651a9b6639d35db5c9208fe7a1ee46368c
stefantanasa <liketanasa@gmail.com>
2021-12-01 15:00:36 +0000

    update

--- M3-d3/app.js
+++ M3-d3/app.js
@@ -15,3 +15,27 @@ const getPhotos = () => {
 };
 
 getPhotos();
+
+let getButton = document.querySelector(".btn.btn-primary.my-2");
+
+getButton.addEventListener("click", getPhotos);
+
+let getThumbnail = document.querySelectorAll(".card.mb-4.shadow-sm");
+
+let svg = getThumbnail[0].firstChild;
+
+let cardImg = document.createElement("img");
+cardImg.className = "bd-placeholder-img card-img-top";
+cardImg.src = "https://pixlr.com/images/index/remove-bg.webp";
+
+getThumbnail[0].appendChild(cardImg);
+
+console.log(svg);
+/*
+1. create new element (img)
+2. add class "bd-placeholder-img card-img-top"
+3. 
+3. select svg ( .firstChild())
+
+
+*/
