b45463cc72fd52a1b866ea7569a4ee0f1d572f45
stefantanasa <liketanasa@gmail.com>
2021-12-01 13:56:22 +0000

    update

--- M3-d3/app.js
+++ M3-d3/app.js
@@ -1,1 +1,11 @@
 {Authorization: "563492ad6f91700001000001bac6f6a96f304864819504d3a10509bf"};
+
+const getPhotos ()=> {
+    fetch("https://api.pexels.com/v1/")
+    .then((response) => response.json())
+    .then((photoData)=> {
+        console.log(photoData)
+    })
+}
+
+getPhotos()
