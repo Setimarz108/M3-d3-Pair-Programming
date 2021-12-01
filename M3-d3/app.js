aef28b7d4425b99bddd1655c479054abb92ad73a
stefantanasa <liketanasa@gmail.com>
2021-12-01 16:26:58 +0000

    second button

--- M3-d3/app.js
+++ M3-d3/app.js
@@ -47,3 +47,30 @@ let getThumbnail = document.querySelectorAll(".col-md-4> .card.mb-4.shadow-sm");
 
 
 */
+const getPhotosSecond = () => {
+  fetch("https://api.pexels.com/v1/search?query=tea", {
+    headers: {
+      Authorization: "563492ad6f91700001000001bac6f6a96f304864819504d3a10509bf",
+    },
+  })
+    .then((response) => response.json())
+    .then((photoData) => {
+      console.log("works");
+      for (let i = 0; i < getThumbnail.length; i++) {
+        // new image
+        console.log(i);
+        let cardImg = document.createElement("img");
+        cardImg.className = "bd-placeholder-img card-img-top";
+        cardImg.src = `${photoData.photos[i].src.medium}`;
+
+        getThumbnail[i].firstChild.remove();
+        getThumbnail[i].replaceChild(cardImg, getThumbnail[i].firstChild);
+        console.log(getThumbnail[i].firstChild);
+      }
+    });
+};
+
+// get the button and add event
+let getSecondButton = document.querySelector(".btn.btn-secondary.my-2");
+console.log(getSecondButton);
+getSecondButton.addEventListener("click", getPhotosSecond);
