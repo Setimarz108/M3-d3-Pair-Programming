b686561b11c9ebd51eba36ff811b93ccb8d8d482
stefantanasa <liketanasa@gmail.com>
2021-12-01 14:16:04 +0000

    update WORKS ! JUMP IN ZOOM! :)

--- M3-d3/app.js
+++ M3-d3/app.js
@@ -1,11 +1,17 @@
-{Authorization: "563492ad6f91700001000001bac6f6a96f304864819504d3a10509bf"};
-
-const getPhotos ()=> {
-    fetch("https://api.pexels.com/v1/")
-    .then((response) => response.json())
-    .then((photoData)=> {
-        console.log(photoData)
-    })
+{
+  Authorization: "563492ad6f91700001000001bac6f6a96f304864819504d3a10509bf";
 }
 
-getPhotos()
+const getPhotos = () => {
+  fetch("https://api.pexels.com/v1/search?query=coffe", {
+    headers: {
+      Authorization: "563492ad6f91700001000001bac6f6a96f304864819504d3a10509bf",
+    },
+  })
+    .then((response) => response.json())
+    .then((photoData) => {
+      console.log(photoData);
+    });
+};
+
+getPhotos();
